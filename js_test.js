193a6c0350234c7fd6a88622d88b2a4646973935
Jon <jonathanjackson424@gmail.com>
2019-10-22 21:37:42 -0400

    am i getting this?

--- js_test.js
+++ js_test.js
@@ -6,3 +6,15 @@ console.log(jon);
 console.log(jon.charAt(5));
 
 //asdfasdfasdfasdfasdf
+
+const dogs = ['golden retriever', 'husky'];
+const cats = ['bengal', 'persian', 'siamese'];
+
+const animals = [...dogs, 'squirrel', ...cats, 'coyote', 'fox'];
+console.log(animals.length);
+
+function getVolume(...dimensions){
+  return dimensions[0] * dimensions[1] * dimensions[2] * dimensions[3];
+}
+
+console.log(getVolume(5, 10, 3, 50));
